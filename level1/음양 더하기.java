d744e1be56c105c0845e4f4156bc0ee509ff4b21
MINSUNG KIM <96581415+6sung@users.noreply.github.com>
2024-04-23 13:37:01 +0900

    Create

---
+++ level1/음양 더하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 123456789;
+        
+        int result=0;
+        for(int i=0;i<absolutes.length;i++){
+            if(signs[i] == true){
+                result+=absolutes[i];
+            }else{
+                result-=absolutes[i];
+            }
+        }
+        answer=result;
+        return answer;
+    }
+}
